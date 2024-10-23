85c8b17558f9b8c649772e9d1df0c93ccf1ad997
balaclar <150714335+balaclar@users.noreply.github.com>
2024-10-23 12:14:53 +0200

    Added models
    Added models

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 
 from pathlib import Path
 
@@ -74,10 +78,25 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'root_pw123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': 
+            {
+         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
