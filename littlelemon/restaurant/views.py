8d9602b049cd519ea750b9d43a9e91be3d80bd32
balaclar <150714335+balaclar@users.noreply.github.com>
2024-11-27 15:10:33 +0200

    Changes on 27/11/2024

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics, viewsets, permissions
-from rest_framework.decorators import api_view
+from rest_framework import generics, viewsets, permissions, status
+from rest_framework.decorators import api_view, renderer_classes, permission_classes, throttle_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser 
+from rest_framework.renderers import JSONRenderer
 from .models import MenuItem, Booking
 from .serializers import MenuItemSerializer,BookingSerializer,UserSerializer
 from django.contrib.auth.models import User, Group
@@ -15,10 +17,19 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     serializer_class = MenuItemSerializer
     
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
     
 class UserViewSet(viewsets.ModelViewSet):
    queryset = User.objects.all()
    serializer_class = UserSerializer
-   permission_classes = [permissions.IsAuthenticated] 
+   permission_classes = [permissions.IsAuthenticated]
+   
+@api_view()
+@permission_classes([IsAuthenticated,IsAdminUser])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
+
