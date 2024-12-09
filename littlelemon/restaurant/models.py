1608c9ee49a3f3a93a8c32ace7dc8f1427b8bc07
balaclar <150714335+balaclar@users.noreply.github.com>
2024-12-09 22:46:47 +0200

    Changes made on 09 Dec 2024

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -15,6 +15,9 @@ class MenuItem(models.Model) :
     price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
     inventory = models.IntegerField()
     
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
+    
     def get_item(self):
         return f'{self.title} : {str(self.price)}'
     
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -5,8 +5,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('menu/items', views.MenuItemsView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu-items/', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('message/', views.msg),
     path('api-token-auth/', obtain_auth_token),
 ]
